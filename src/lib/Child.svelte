057a71cfad1c3fb5024412076a20234780d4802d
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2022-01-31 17:59:36 -0800

    Initial commit

---
+++ src/lib/Child.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let name: string = '';
+</script>
+
+<h2>Hello, {name}</h2>
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Parent from '$lib/Parent.svelte';
+
+	function name() {
+		return 'Alice';
+	}
+</script>
+
+<Parent {name} />
